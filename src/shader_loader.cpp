c975d42622cb13897861119413c756edf3d7ee57
baptiste boulan <boulanbaptiste1@gmail.com>
2025-03-24 20:58:43 +0100

    cleaned the shaders

--- src/shader_loader.cpp
+++ src/shader_loader.cpp
@@ -1,42 +1,59 @@
 #include "shader_loader.h"
 
-unsigned int make_module(const std::string file_path, unsigned int module_type) {
-    std::ifstream file;
-    std::string line;
-
-    std::stringstream bufferedlines;
-
-    file.open(file_path);
-
-    while (std::getline(file, line)) {
-        bufferedlines << line << '\n';
+GLint make_module(const std::string& file_path, GLint module_type, const std::string& include_path) {
+    std::ifstream file(file_path);
+    if (!file.is_open()) {
+        std::cerr << "Erreur : Impossible d'ouvrir le fichier " << file_path << std::endl;
+        return 0;
     }
 
-    std::string shaderSource = bufferedlines.str();
-
-    const char* shaderSrc = shaderSource.c_str();
-    bufferedlines.str("");
+    std::stringstream main_shader;
+    std::string line;
+    int line_number = 0;
+    
+    // Read the first shader file
+    while (std::getline(file, line)) {
+        main_shader << line << '\n';
+        if (line_number == 20 && !include_path.empty()) {
+            std::ifstream include_file(include_path);
+            if (include_file.is_open()) {
+                std::stringstream include_shader;
+                include_shader << include_file.rdbuf();
+                main_shader << include_shader.str() << '\n';
+                include_file.close();
+            } else {
+                std::cerr << "Erreur : Impossible d'ouvrir le fichier d'inclusion " << include_path << std::endl;
+            }
+        }
+        line_number += 1 ;
+    }
     file.close();
 
-    unsigned int shaderModule = glCreateShader(module_type); 
+
+    std::string shaderSource = main_shader.str();
+    const char* shaderSrc = shaderSource.c_str();
+
+
+    GLint shaderModule = glCreateShader(module_type);
     glShaderSource(shaderModule, 1, &shaderSrc, NULL);
     glCompileShader(shaderModule);
 
     int success;
     glGetShaderiv(shaderModule, GL_COMPILE_STATUS, &success);
     if (!success) {
-        char errorLog[1024]; //size of the error log
+        char errorLog[1024];
         glGetShaderInfoLog(shaderModule, 1024, NULL, errorLog);
-        std::cout<<"Shader Module error"<< errorLog << std::endl;
+        std::cerr << "Erreur de compilation du shader : " << errorLog << std::endl;
     }
 
     return shaderModule;
 }
 
-unsigned int make_shader(const std::string& vert_path, const std::string& frag_path) {
+GLint make_shader(const std::string& vert_path, const std::string& frag_path) {
     std::vector<unsigned int> modules;
     modules.push_back(make_module(vert_path,GL_VERTEX_SHADER));
-    modules.push_back(make_module(frag_path,GL_FRAGMENT_SHADER));
+    // modules.push_back(make_module(frag_path,GL_FRAGMENT_SHADER));
+    modules.push_back(make_module(frag_path,GL_FRAGMENT_SHADER,"./shaders/skyCOlor.glsl"));
 
     unsigned int shader = glCreateProgram();
     for (unsigned int module : modules) {
--- src/shader_loader.h
+++ src/shader_loader.h
@@ -1,4 +1,4 @@
 #include "config.h"
 
-unsigned int make_module(const std::string file_path, unsigned int module_type);
-unsigned int make_shader(const std::string& vert_path, const std::string& frag_path);
+GLint make_module(const std::string& file_path, GLint module_type, const std::string& include_path = "");
+GLint make_shader(const std::string& vert_path, const std::string& frag_path);
